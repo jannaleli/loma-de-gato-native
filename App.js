d617bd0090efdb8aed398461c8bd1c6d791e1f01
Aleli <jann.aleli@gmail.com>
2021-01-28 09:06:49 +0800

    First Commit for Loma De Gato Native

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent:'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Username"
+        style={{ width: '80%', borderColor:'black', borderWidth:1, padding:10}} />
+
+       
+        <Button title="Login" />
+      </View>
+      
     </View>
   );
 }
